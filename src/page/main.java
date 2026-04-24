e3e7f6f64e5e16033a9c3c0ccd400686e4e401e4
2-Rah <harukamasta@gmail.com>
2026-04-24 22:50:28 +0800

    project login sign-in dan login java

---
+++ src/page/main.java
@@ -0,0 +1,24 @@
+package page;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author 2RAH
+ */
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Buat Objek untuk login
+        page.login halamanUtama = new page.login();
+        // menampilkan objek
+        halamanUtama.setVisible(true);
+    }
+    
+}
